9136654ead1627d6430355530248898ad1f86320
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2019-08-02 00:09:16 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-call pkg index.js --targets node10-win-x64 --output .\bin\win\conceal-services.exe
-call pkg index.js --targets node10-linux-x64 --output .\bin\linux\conceal-services
-call pkg index.js --targets node10-macos-x64 --output .\bin\macos\conceal-services
+call pkg index.js --targets node10-win-x64 --output .\bin\win\SmartCryptoTech-services.exe
+call pkg index.js --targets node10-linux-x64 --output .\bin\linux\SmartCryptoTech-services
+call pkg index.js --targets node10-macos-x64 --output .\bin\macos\SmartCryptoTech-services
